5759b4c8224bc898b7e9a5fb38b67ce2a05da595
cschweighofer <cschweighofe@student.ethz.ch>
2025-12-04 15:56:24 +0100

    android init

---
+++ frontend/android/app/src/main/java/com/routify/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.routify.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.routify.app',
+  appName: 'Routify',
+  webDir: 'dist/routify'
+};
+
+export default config;
